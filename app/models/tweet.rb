122a1816a64a8dab0240c8208de2e01d3ef882ff
Alan Middleton <alanmiddleton@yahoo.com>
2012-07-25 11:26:31 -0600

    Added a Notes Controller and created index, show, new, create, edit, update and destroy actions.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,7 +6,10 @@ class Tweet < ActiveRecord::Base
   validates(:tweeted_at, :presence => true)
   validates(:user_id, :presence => true,)
 
+  scope(:with_notes, includes(:notes).order('notes.created_at desc'))
+
   belongs_to(:user)
+  has_many(:notes)
   has_and_belongs_to_many(:categories)
 
   scope(:recent, order('tweeted_at desc'))
